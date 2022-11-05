6e1347f265ab64441d8b8041f1c74ae85d412915
JoanClaverol <41630699+JoanClaverol@users.noreply.github.com>
2022-11-05 17:31:52 +0100

    added edit and save functionalities

--- todo.js
+++ todo.js
@@ -5,7 +5,7 @@ function getTask() {
     }
     // append task to list
     let taskItem = document.createElement('div');
-    taskItem.innerHTML = '<div id="task" class="m-2" style="display: flex; justify-content: space-between; align-items: center;"><div>' + task + '</div><div class="btn-group" id="btnGroup"><button class="btn btn-danger" type="button" id="deleteButton" onclick="deleteTask(this)">Delete</button><button class="btn btn-light" type="button" onclick="editTask()">Edit</button></div></div>';
+    taskItem.innerHTML = '<div id="task" class="m-2" style="display: flex; justify-content: space-between; align-items: center;"><div>' + task + '</div><div class="btn-group" id="btnGroup"><button class="btn btn-danger" type="button" id="deleteButton" onclick="deleteTask(this)">Delete</button><button class="btn btn-light" type="button" id="editButton" onclick="editTask(this)">Edit</button></div></div>';
     document.getElementById('taskList').appendChild(taskItem);
     // clear input
     document.getElementById('inputTask').value = '';
@@ -21,5 +21,22 @@ function editTask(element) {
     // edit task
     let task = element.parentNode.parentNode.getElementsByTagName('div')[0];
     let originTask = task.innerHTML;
-    element.parentNode.parentNode.getElementsByTagName('div')[0].innerHTML = '<input type="text" id="editTask" value="' + originTask + '">';;
+    element.parentNode.parentNode.getElementsByTagName('div')[0].innerHTML = '<input type="text" class="form-control" id="editTask" value="' + originTask + '">';
+    element.parentNode.innerHTML = '<button class="btn btn-success" type="button" id="saveButton" onclick="saveTask(this)">Save</button><button class="btn btn-light" type="button" onclick="cancelTask(this, ' + originTask + ')">Cancel</button>';
 };
+
+function saveTask(element) {
+    let task = element.parentNode.parentNode.getElementsByTagName('div')[0];
+    let originTask = task.getElementsByTagName('input')[0].value;
+    if (originTask === '') {
+        deleteTask(element);
+    }
+    task.innerHTML = originTask;
+    element.parentNode.innerHTML = '<button class="btn btn-danger" type="button" id="deleteButton" onclick="deleteTask(this)">Delete</button><button class="btn btn-light" type="button" onclick="editTask(this)">Edit</button>';
+};
+
+function cancelTask(element, originTask) {
+    let task = element.parentNode.parentNode.getElementsByTagName('div')[0];
+    task.innerHTML = originTask;
+    element.parentNode.innerHTML = '<button class="btn btn-danger" type="button" id="deleteButton" onclick="deleteTask(this)">Delete</button><button class="btn btn-light" type="button" onclick="editTask(this)">Edit</button>';
+}
